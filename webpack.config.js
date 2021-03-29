5cde777de3bd95b827f47dc605a14d9a724b1894
Roharui <jack555586@gmail.com>
2021-03-29 18:21:21 +0900

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: path.resolve(__dirname, "node_modules"),
+      },
+    ],
+  },
+  devtool: "source-map",
+  target: "node",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
